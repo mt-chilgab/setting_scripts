814a33c46291a425dc885f15cce7029d601f5ca8
mt-chilgab <tireburner1999@gmail.com>
2022-03-17 15:25:54 +0900

    added dependencies install

--- setof.sh
+++ setof.sh
@@ -2,6 +2,10 @@
 
 pwd_=$PWD
 
+echo "Installing dependencies..."
+sudo apt-get update
+sudo apt-get -y install build-essential cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot libreadline-dev libncurses5-dev libxt-dev libopenmpi-dev openmpi-bin libboost-system-dev libboost-thread-dev libgmp-dev libmpfr-dev python python-dev libcgal-dev libglu1-mesa-dev libqt4-opengl-dev
+
 echo "Setting OpenFOAM env vars..."
 source /opt/OpenFOAM/OpenFOAM-2.3.1/etc/bashrc $FOAM_SETTINGS
 
