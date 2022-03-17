b611540705b224baab9fa6dfe3705e4801100ef5
mt-chilgab <tireburner1999@gmail.com>
2022-03-17 18:24:49 +0900

    fixed

--- setof.sh
+++ setof.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 pwd_=$PWD
 
@@ -7,16 +7,23 @@ sudo apt-get update
 sudo apt-get -y install build-essential cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot libreadline-dev libncurses5-dev libxt-dev libopenmpi-dev openmpi-bin libboost-system-dev libboost-thread-dev libgmp-dev libmpfr-dev python python-dev libcgal-dev libglu1-mesa-dev libqt4-opengl-dev
 
 echo "Setting DISPLAY variable and setting LIBGL_ALWAYS_INDIRECT to 1"
-if ! grep -q "export\sDISPLAY=\$\(.+nameserver.+resolv\.conf.+:\d" ~/.bashrc; then
-	export DISPLAY=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0
+if ! grep -q "export\sDISPLAY=.+nameserver.+resolv\.conf.+:\d" ~/.bashrc; then
+	echo "export DISPLAY=\$(grep -m 1 nameserver /etc/resolv.conf | awk '{print \$2}'):0" >> ~/.bashrc
 else
 	echo "DISPLAY var exists!"
 fi
 
 if ! grep -q "export\sLIBGL_ALWAYS_INDIRECT=\d" ~/.bashrc; then
-	export LIBGL_ALWAYS_INDIRECT=1
+	echo "export LIBGL_ALWAYS_INDIRECT=1" >> ~/.bashrc
 else
 	echo "LIBGL_ALWAYS_INDIRECT is already set!"
+fi
+
+echo "Adding Salome and Helyx-OS to PATH..."
+if ! grep -q ".+PATH.+salome.+HELYX-OS" ~/.bashrc; then
+	echo "export PATH=\$PATH:/opt/salome/appli_V7_4_0:/opt/Engys/HELYX-OS/v2.3.1" >> ~/.bashrc
+else
+	echo "Already exists in PATH!"
 fi
 
 echo "Adding alias setof231 to .bashrc..."
@@ -28,7 +35,10 @@ fi
 
 echo "Adding alias of231 to .bashrc..."
 if ! grep -q "alias\sof231=" ~/.bashrc; then
-	echo "alias of231=\"source /opt/OpenFOAM/OpenFOAM-2.3.1/etc/bashrc $FOAM_SETTINGS\"" >> ~/.bashrc
+	echo "alias of231=\"source /opt/OpenFOAM/OpenFOAM-2.3.1/etc/bashrc \$FOAM_SETTINGS\"" >> ~/.bashrc
+else
+	echo "Alias already exists!"
+fi
 
 echo "Making FOAM_RUN and extension directory if they don't exist..." 
 [ ! -d "$FOAM_RUN/../OpenFOAM_extensions" ] && mkdir -p $FOAM_RUN/../OpenFOAM_extensions
@@ -43,8 +53,6 @@ if [ ! -d "$FOAM_RUN/../OpenFOAM_extensions/vimExtensionOpenFOAM" ]; then
 else
 	echo "Already installed!"
 fi
-
-source ~/.bashrc
 
 echo "Finished"
 cd $pwd_
